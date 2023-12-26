50100d108ab1e947b629806f4c338a70d5ec74b9
Wilke Klausing <klausing@campus.tu-berlin.de>
2023-12-26 16:07:36 +0100

    Need more work on Sabre

--- sabreEnv/tests/sabreTests/sabreNetworkTest.py
+++ sabreEnv/tests/sabreTests/sabreNetworkTest.py
@@ -1,38 +1,39 @@
 import unittest
 
-from sabreEnv.sabre.sabreV6 import Sabre as SabreV6
+#from sabreEnv.sabre.sabreV6 import Sabre as SabreV6
 from sabreEnv.sabre.sabreV8 import Sabre as SabreV8
+from sabreEnv.sabre.sabreV8 import Sabre as SabreV9
 
 class TestMainFunction(unittest.TestCase):
 
-    def testSabreV8(self):
+    def testSabreV8Network(self):
         '''
         Testing sabreV6.py against sabreV8.py, but with different network traces which should have the exact same results.
-
-        TODO: 'bola', 
         '''        
-        abrList = ['bolae']# , 'throughput', 'dynamic', 'dynamicdash'
-        averageList = ['ewma']#, 'sliding'
+        abrList = ['bolae']# 'bola', 'throughput', 'dynamic', 'dynamicdash' 
+        averageList = ['ewma']# ', 'sliding'
         for abr in abrList:
             for average in averageList:
                 print('Testing: ', abr, average)
                 resultSabreV8 = SabreV8(abr=abr, moving_average=average, verbose=False,  replace='right').testing(network='sabreEnv/sabre/data/networkTest1.json')
-                resultSabreV8Sec = SabreV8(abr=abr, moving_average=average, verbose=False,  replace='right').testing(network='sabreEnv/sabre/data/networkTest2.json')# 471
-                print('Now for Second Test')
-                
-                
+                resultSabreV8Sec = SabreV8(abr=abr, moving_average=average, verbose=False,  replace='right').testing(network='sabreEnv/sabre/data/networkTest2.json')
                 for key in resultSabreV8:
                     print('key: ', key)
-                    #self.assertEqual(resultSabreV8[key], resultSabreV8Sec[key])
+                    self.assertEqual(resultSabreV8[key], resultSabreV8Sec[key])
                     #self.assertAlmostEqual(resultSabreV8[key], resultSabreV8Sec[key],1)
-                    self.assertEqual(1, 1)
+
+    def testSabreV9_regression(self):
+        abrList = ['bolae']# 'bola', 'throughput', 'dynamic', 'dynamicdash' 
+        averageList = ['ewma']# ', 'sliding'
+        for abr in abrList:
+            for average in averageList:
+                print('Testing: ', abr, average)
+                resultSabreV8 = SabreV8(abr=abr, moving_average=average, verbose=False,  replace='right').testing(network='sabreEnv/sabre/data/networkTest1.json')
+                resultSabreV9 = SabreV9(abr=abr, moving_average=average, verbose=False,  replace='right').testing(network='sabreEnv/sabre/data/networkTest1.json')
+                for key in resultSabreV8:
+                    #print('key: ', key)
+                    self.assertEqual(resultSabreV8[key], resultSabreV9[key])
                          
 
 if __name__ == '__main__':
     unittest.main()
-
-    # SabreV8       229
-    # SabreV8Sec    242
-
-    {'status': 'completed', 'buffer_size': 25000, 'total_played_utility': 437.36238530806844, 'time_average_played_utility': 2.1968726071655786, 'total_played_bitrate': 438411, 'time_average_played_bitrate': 2202.139801989736, 'total_play_time': 597.2522720000001, 'total_play_time_chunks': 199.0840906666667, 'total_rebuffer': 0.0, 'rebuffer_ratio': 0.0, 'time_average_rebuffer': 0.0, 'total_rebuffer_events': 0, 'time_average_rebuffer_events': 0.0, 'total_bitrate_change': 20907, 'time_average_bitrate_change': 105.01592533079555, 'total_log_bitrate_change': 12.7743049135312, 'time_average_log_bitrate_change': 0.06416537288717689, 'time_average_score': 2.1968726071655786, 'total_reaction_time': 186.01817600000032, 'estimate': -284.4087415001153}
-    {'status': 'completed', 'buffer_size': 25000, 'total_played_utility': 437.36238530806844, 'time_average_played_utility': 2.1968726071655786, 'total_played_bitrate': 438411, 'time_average_played_bitrate': 2202.139801989736, 'total_play_time': 597.2522720000001, 'total_play_time_chunks': 199.0840906666667, 'total_rebuffer': 0.0, 'rebuffer_ratio': 0.0, 'time_average_rebuffer': 0.0, 'total_rebuffer_events': 0, 'time_average_rebuffer_events': 0.0, 'total_bitrate_change': 20907, 'time_average_bitrate_change': 105.01592533079555, 'total_log_bitrate_change': 12.7743049135312, 'time_average_log_bitrate_change': 0.06416537288717689, 'time_average_score': 2.1968726071655786, 'total_reaction_time': 186.01817600000032, 'estimate': -284.4087415001153}
